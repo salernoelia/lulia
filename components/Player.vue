5aa5da5ab7c728d302a6900162099a53802b66a8
salernoelia <putopet@gmail.com>
2023-07-19 17:35:03 +0200

    feat: walking sound

--- components/Player.vue
+++ components/Player.vue
@@ -1,18 +1,19 @@
 <template>
-    <div>
-        <Console :values="{ x }" />
-        <div class="player" :style="{ top: player.y + 'px', left: x + 'px'}" />
-    </div>
+	<div>
+		<Console :values="{ x }" />
+		<div class="player" :style="{ top: player.y + 'px', left: x + 'px' }" />
+	</div>
 </template>
 
 <script setup>
+const walking = useSound("/sounds/walk.mp3")
 const player = reactive({
-    x: 0,
-    y: 0,
-    speed: 10,
-    direction: 'right',
-    sprite: '/sprites/stand.png',
-    name: 'Player1'
+	x: 0,
+	y: 0,
+	speed: 10,
+	direction: "right",
+	sprite: "/sprites/stand.png",
+	name: "Player1",
 })
 
 const test = ref(100)
@@ -20,35 +21,39 @@ const x = computed(() => test.value - 70)
 const y = computed(() => player.y)
 const { element } = useElementByPoint({ x, y })
 
-onKeyStroke(['w', 'W', 'ArrowUp'], (e) => {
-  e.preventDefault()
-  player.y -= player.speed
+onKeyStroke(["w", "W", "ArrowUp"], (e) => {
+	e.preventDefault()
+	player.y -= player.speed
+	walking.play()
 })
 
-onKeyStroke(['d', 'D', 'ArrowRight'], (e) => {
-  e.preventDefault()
-  console.log(element.value)
-  if (element.value?.className !== 'gleis') test.value += player.speed
+onKeyStroke(["d", "D", "ArrowRight"], (e) => {
+	e.preventDefault()
+	console.log(element.value)
+	if (element.value?.className !== "gleis") test.value += player.speed
+	walking.play()
 })
 
-onKeyStroke(['s', 'S', 'ArrowDown'], (e) => {
-  e.preventDefault()
-  player.y += player.speed
+onKeyStroke(["s", "S", "ArrowDown"], (e) => {
+	e.preventDefault()
+	player.y += player.speed
+	walking.play()
 })
 
-onKeyStroke(['a', 'A', 'ArrowLeft'], (e) => {
-  e.preventDefault()
-  test.value -= player.speed
+onKeyStroke(["a", "A", "ArrowLeft"], (e) => {
+	e.preventDefault()
+	test.value -= player.speed
+	walking.play()
 })
 </script>
 
 <style scoped>
 .player {
-    background-color: black;
-    width: 50px;
-    height: 50px;
-    border-radius: 50%;
-    position: absolute;
-    transition: all 150ms;
+	background-color: black;
+	width: 50px;
+	height: 50px;
+	border-radius: 50%;
+	position: absolute;
+	transition: all 150ms;
 }
 </style>
---
+++ pages/sublevel.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="sublevel">
+		<Player />
+	</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.sublevel {
+	width: 800px;
+	height: 300px;
+	background-color: gray;
+}
+</style>
