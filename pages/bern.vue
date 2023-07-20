76551b9323a5d1c8af3d8d8d4b90af33bfb0e8fb
salernoelia <putopet@gmail.com>
2023-07-20 13:40:13 +0200

    feat: gleisschilder + train-screen

--- pages/bern.vue
+++ pages/bern.vue
@@ -1,12 +1,19 @@
 <template>
-    <div flex flex-row w-screen h-screen overflow-hidden>
-        <div flex flex-col w-100vw>
-            <template v-for="n in 12">
-                <input w-200% ml--50% class="zug" :class="'zug-' + n" type="range" step="0.01" :value="n === 15 ? 100 : 0" />
-                <div class="perron" bg-gray h-50px>gleis {{ n }}</div>
-            </template>
-        </div>
-    </div>
+	<div flex flex-row w-screen h-screen overflow-hidden>
+		<div flex flex-col w-100vw>
+			<template v-for="n in 12">
+				<input w-200% ml--50% class="zug" :class="'zug-' + n" type="range" step="0.01" :value="n === 15 ? 100 : 0" />
+				<div class="perron" h-50px>
+					<div class="gleis-schild">
+						<div class="gleis-schild-innen">
+							<p class="gleis-text-kl">Gleis</p>
+							<h1 class="gleis-text-gr">{{ n }}</h1>
+						</div>
+					</div>
+				</div>
+			</template>
+		</div>
+	</div>
 </template>
 
 <script setup>
@@ -14,25 +21,37 @@ const abfahrt = useSound("/sounds/abfahrt.mp3")
 const einfahrt = useSound("/sounds/einfahrt.m4a")
 
 const abfahren = () => {
-    abfahrt.seek(3)
-    abfahrt.play()
-    useAnime({ targets: '.zug-3', value: 100, duration: 20000, delay: 5000, easing: 'easeInQuad' })
+	abfahrt.seek(3)
+	abfahrt.play()
+	useAnime({ targets: ".zug-3", value: 100, duration: 20000, delay: 5000, easing: "easeInQuad" })
 }
 
 const einfahren = () => {
-    einfahrt.play()
-    console.log('einfahren')
-    useAnime({ targets: '.zug-15', value: 0, duration: 20000, easing: 'easeOutQuad' })
+	einfahrt.play()
+	console.log("einfahren")
+	useAnime({ targets: ".zug-15", value: 0, duration: 20000, easing: "easeOutQuad" })
 }
 
-onKeyStroke(['a', 'A'], (e) => {
-  e.preventDefault()
-  abfahren()   
+onKeyStroke(["a", "A"], (e) => {
+	e.preventDefault()
+	abfahren()
 })
 
-onKeyStroke(['e', 'E'], (e) => {
-  e.preventDefault()
-  einfahren()   
+onKeyStroke(["e", "E"], (e) => {
+	e.preventDefault()
+	einfahren()
 })
-
 </script>
+
+<style scoped>
+.gleis-text-kl {
+	margin: 1px 0 0 2px;
+	font-size: 10px;
+}
+.gleis-text-gr {
+	margin: 0;
+	padding: 0;
+	font-size: 23px;
+	text-align: center;
+}
+</style>
---
+++ pages/train-screen.vue
@@ -0,0 +1,58 @@
+<template>
+	<div>
+		<div class="train-screen">
+			<!-- <div class="train-screen__element">
+				<h1 class="train-screen__element--title">Zug</h1>
+				<h1 class="train-screen__element--time">Abfahrt</h1>
+				<h1 class="train-screen__element--locations">Strecke</h1>
+				<h1 class="train-screen__element--rail">Gleis</h1>
+				<h1 class="train-screen__element--notice">Hinweis</h1>
+			</div> -->
+			<div v-for="n in 14" class="train-screen__element">
+				<!-- <h1 :style="[IR ? `background-color: #fff; color: #000;` : `background-color: #ff0000; color: #fff;`]" class="train-screen__element--title">IR 3</h1> -->
+				<h1 :style="{ 'background-color': IR ? 'red' : 'white', color: IR ? 'white' : 'black' }" class="train-screen__element--title">IR 3</h1>
+				<h1 class="train-screen__element--time">19:03</h1>
+				<h1 class="train-screen__element--locations">Zürich HB - Bern</h1>
+				<h1 class="train-screen__element--rail">23</h1>
+				<h1 class="train-screen__element--notice">-</h1>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+const IR = ref(true)
+</script>
+
+<style lang="scss" scoped>
+h1 {
+	margin: 0;
+}
+.train-screen {
+	display: flex;
+	flex-direction: column;
+	color: white;
+	width: 80vw;
+	height: 80vh;
+	background-color: #26325c;
+	border: 5px solid #18203e;
+}
+
+.train-screen__element {
+	border-bottom: white solid 1px;
+	padding-bottom: 10px;
+	padding-top: 10px;
+	padding-left: 20px;
+	padding-right: 20px;
+	display: flex;
+	flex-direction: row;
+	justify-content: left;
+	gap: 40px;
+
+	height: 50px;
+}
+.train-screen__element--title {
+	margin: 0;
+	width: 80px;
+}
+</style>
